746c97d3bce3d58bde4bb828923835c507087cf0
Renan Gomes <renango2015@gmail.com>
2023-12-23 08:38:21 -0300

    refactor: add category reset to useStateReset

--- src/app/hooks/useSearchbar.ts
+++ src/app/hooks/useSearchbar.ts
@@ -2,14 +2,13 @@ import { useContext } from '../context/Context';
 import { useStateReset } from './useStateReset';
 
 export const useSearchbar = () => {
-  const { productsInitialState, setProducts, setSelectedCategory } = useContext();
+  const { productsInitialState, setProducts } = useContext();
   const { stateReset } = useStateReset();
 
   const handleSearchbar = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value: search } = event.target;
 
-    setSelectedCategory('filterByAll');
-    stateReset(['Pagination', 'Dropdown']);
+    stateReset(['Category', 'Pagination', 'Dropdown']);
 
     setProducts(() =>
       productsInitialState.filter(({ name }) => {
--- src/app/hooks/useStateReset.ts
+++ src/app/hooks/useStateReset.ts
@@ -1,22 +1,14 @@
 import { useContext } from '../context/Context';
 
-type UseStateReset = ('Pagination' | 'Dropdown')[];
+type UseStateReset = ('Category' | 'Pagination' | 'Dropdown')[];
 
 export const useStateReset = () => {
-  const { setPaginationState, setDropdownMenuState } = useContext();
+  const { setSelectedCategory, setPaginationState, setDropdownMenuState } = useContext();
 
   const stateResetInitialValues = {
-    Pagination: () =>
-      setPaginationState(() => ({
-        page: 1,
-        range: [0, 12],
-        buttons: [1, 2, 3, 4, 5],
-      })),
-    Dropdown: () =>
-      setDropdownMenuState(() => ({
-        isDropdownToggled: false,
-        sortingBy: 'Organizar por',
-      })),
+    Category: () => setSelectedCategory('filterByAll'),
+    Pagination: () => setPaginationState(() => ({ page: 1, range: [0, 12], buttons: [1, 2, 3, 4, 5] })),
+    Dropdown: () => setDropdownMenuState(() => ({ isDropdownToggled: false, sortingBy: 'Organizar por' })),
   };
 
   const stateReset = (toReset: UseStateReset) => {
