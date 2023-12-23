fb9c06e60794a15c52624fa87f296219bc867c9e
Renan Gomes <renango2015@gmail.com>
2023-12-23 19:17:32 -0300

    style: update loader styles

--- src/app/components/Loader/Loader.tsx
+++ src/app/components/Loader/Loader.tsx
@@ -2,5 +2,5 @@ import styles from './Loader.module.scss';
 import Image from 'next/image';
 
 export const Loader = () => {
-  return <Image className={styles.loader} src='./3-dots-move.svg' alt='loader' width={75} height={75} />;
+  return <Image className={styles.loader} src='/3-dots-move.svg' alt='loader' width={75} height={75} />;
 };
