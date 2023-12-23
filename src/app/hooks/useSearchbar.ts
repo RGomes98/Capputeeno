94dbe1fc03bdf9d5ce96120859315d46ee4120be
Renan Gomes <renango2015@gmail.com>
2023-12-23 19:23:23 -0300

    fix: userSearchbar not redirecting to home on user input

--- src/app/hooks/useSearchbar.ts
+++ src/app/hooks/useSearchbar.ts
@@ -1,13 +1,19 @@
+import { usePathname, useRouter } from 'next/navigation';
 import { useContext } from '../context/Context';
 import { useStateReset } from './useStateReset';
 
 export const useSearchbar = () => {
   const { productsInitialState, setProducts } = useContext();
   const { stateReset } = useStateReset();
+  const { push } = useRouter();
+  const path = usePathname();
+
+  const isAtHome = path === '/';
 
   const handleSearchbar = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value: search } = event.target;
 
+    if (!isAtHome) push('/');
     stateReset(['Category', 'Pagination', 'Dropdown']);
 
     setProducts(() =>
