364cff21ce433a15912e5be79b09990d3d9845d0
Renan Gomes <renango2015@gmail.com>
2023-12-26 17:40:40 -0300

    refactor: increase loader priority

--- src/app/components/Loader/Loader.tsx
+++ src/app/components/Loader/Loader.tsx
@@ -2,5 +2,7 @@ import styles from './Loader.module.scss';
 import Image from 'next/image';
 
 export const Loader = () => {
-  return <Image className={styles.loader} src='/3-dots-move.svg' alt='loader' width={75} height={75} />;
+  return (
+    <Image className={styles.loader} priority src='/3-dots-move.svg' alt='loader' width={75} height={75} />
+  );
 };
